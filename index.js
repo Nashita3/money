b7543b744eaf7482d3945496b36da622802682e7
Nashita3 <takanashi.kousei@gmail.com>
2020-05-12 22:45:27 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+// phina.js をグローバル領域に展開
+phina.globalize();
+
+const SCREEN_WIDTH = 640; // 横幅
+const SCREEN_HEIGHT = 960; // 縦幅
+
+// MainScene クラスを定義
+phina.define('MainScene', {
+  superClass: 'DisplayScene',
+  init: function() {
+    this.superInit();
+    // 背景色を指定
+    this.backgroundColor = '#444';
+    let shape = Shape().addChildTo(this);
+    shape.backgroundColor = 'white';
+
+    shape.x = 320;
+    shape.y = 480;
+    shape.update = function(e) {
+        // 移動
+        shape.x = e.pointer.x;
+        shape.y = e.pointer.y;
+    };
+  },
+});
+// メイン処理
+phina.main(function() {
+  // アプリケーション生成
+  const app = GameApp({
+    startLabel: 'main', // メインシーンから開始する
+    width: SCREEN_WIDTH, // 横幅
+    height: SCREEN_HEIGHT, // 縦幅
+  });
+  // アプリケーション実行
+  app.run();
+});
